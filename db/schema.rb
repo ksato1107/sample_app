3cfe8daee82de8ccb5278ff774d5f962eab37176
vagrant <vagrant@localhost.localdomain>
2018-03-20 04:57:21 +0000

    Make a Micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311043139) do
+ActiveRecord::Schema.define(version: 20180320044515) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
