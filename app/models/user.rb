669e558bdcb970579137f55e0c11284926595d60
vagrant <vagrant@localhost.localdomain>
2018-03-20 07:00:20 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  def feed
+   Micropost.where("user_id = ?", id)
+  end
+  
   has_many :microposts, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
