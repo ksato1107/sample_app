1d94c6f9388a916f9dc3c78963202935d48c74f4
vagrant <vagrant@localhost.localdomain>
2018-03-20 09:04:22 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   def feed
-   Micropost.where("user_id = ?", id)
+   Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
                                    class_name:  "Relationship",
                                    dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, length: { maximum: 50 }
